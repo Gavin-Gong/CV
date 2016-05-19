8f448cb1c0452a2510f4f0fafb06cdfd38500d28
Gavin Gong <kingdompluto@gmail.com>
2016-05-19 21:02:02 +0800

    edu-box待修复

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 // 设置定高函数 FSHeight == Full Screen Height
-	function setFSHeight(Elements, minusHeight) {
+	function setFSHeight(Elements) {
 		// viewport height
 		var FS = window.innerHeight;
 		// set height by loop
 		for (var i = 0, len = Elements.length; i < len; i++) {
-			Elements[i].style.height = (FS - minusHeight) + 'px';
+			Elements[i].style.height = FS + 'px';
 		}
 	}
 
-
-
 	var boxes = document.getElementsByClassName("box");
 	// window.addEventListener("resize", setFSHeight(boxes, 60), false); 这样是不行的, 函参数只能只能
+	setFSHeight(boxes);
 	window.addEventListener("resize", function() {
-		setFSHeight(boxes, 30);
+		setFSHeight(boxes);
 	}, false);
+
